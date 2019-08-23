880fb5568f3b20551b375d7b5e0f93bb1ba8d122
Ryan Jasinski <jasinski.17@gmail.com>
2019-08-23 17:00:32 -0400

    fixed feedback 2

--- server/database/models/EthereumWallet.js
+++ server/database/models/EthereumWallet.js
@@ -2,10 +2,6 @@ const Sequelize = require('./node_modules/sequelize');
 const db = require('../db');
 
 const EthereumWallet = db.define('ethereumWallet', {
-    userId: {
-        type: Sequelize.INTEGER,
-        allowNull:false
-    },
     balance: {
         type: Sequelize.DECIMAL(10,2),
         allowNull: false
