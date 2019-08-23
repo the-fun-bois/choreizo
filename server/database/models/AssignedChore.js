02aa88fee3c5587ed3958eab4b35ad38314edac7
Ryan Jasinski <jasinski.17@gmail.com>
2019-08-23 17:01:50 -0400

    fixed feedback 4

--- server/database/models/AssignedChore.js
+++ server/database/models/AssignedChore.js
@@ -2,10 +2,6 @@ const Sequelize = require('./node_modules/sequelize');
 const db = require('../db');
 
 const AssignedChore = db.define('assignedChores', {
-    assignedChoreId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-    },
     completed:{
         type: Sequelize.ENUM(['accepted', 'declined', 'pending']),
         defaultValue: 'pending',
