a5e4af537b032a079d7d62165ef97482e0f85a5a
Ryan Jasinski <jasinski.17@gmail.com>
2019-08-25 15:29:26 -0400

    ChoresRoutes connected

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -4,9 +4,14 @@ const apiRoutes = express.Router();
 const tradeChoreRoutes = require('./tradeChoreRoutes');
 const swapChoreRoutes = require('./swapChoreRoutes');
 const transferChoreRoutes = require('./transferChoreRoutes');
+
+const choresAPIRoutes = require('./chores');
+
 // set api routes here
 apiRoutes.use('/trade_chore', tradeChoreRoutes);
 apiRoutes.use('/swap_chore', swapChoreRoutes);
 apiRoutes.use('/transfer_chore', transferChoreRoutes);
+
+apiRoutes.use('/chores', choresAPIRoutes);
 
 module.exports = apiRoutes;
