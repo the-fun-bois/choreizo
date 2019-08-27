0bec31f0a80e05ca09347c3e7a5fe0ae4574611b
Ryan Jasinski <jasinski.17@gmail.com>
2019-08-27 19:51:21 -0400

    Chores Routes Working

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const apiRoutes = express.Router();
 //import api routes here
-<<<<<<< HEAD
 
-const choresAPIRoutes = require('./ChoresRoutes');
-
-=======
 const tradeChoreRoutes = require('./tradeChoreRoutes');
 const swapChoreRoutes = require('./swapChoreRoutes');
 const transferChoreRoutes = require('./transferChoreRoutes');
->>>>>>> 2c69341bf83198c0ea26000deac9268dc54e1140
+const choresRoutes = require('./ChoresRoutes');
 // set api routes here
 apiRoutes.use('/trade_chore', tradeChoreRoutes);
 apiRoutes.use('/swap_chore', swapChoreRoutes);
 apiRoutes.use('/transfer_chore', transferChoreRoutes);
 
-apiRoutes.use('/chores', choresAPIRoutes);
+apiRoutes.use('/chores', choresRoutes);
 
 module.exports = apiRoutes;
