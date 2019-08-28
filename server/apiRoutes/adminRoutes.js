7dbb2aebb839aafc634eda51e67e13b37746fb60
Ryan Jasinski <jasinski.17@gmail.com>
2019-08-28 07:47:20 -0400

    /create_group route complete

--- server/apiRoutes/adminRoutes.js
+++ server/apiRoutes/adminRoutes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
-const { Group } = require('../database/index');
+const { Group, UserGroup, Chore } = require('../database/index');
 
 /*
  * @ROUTE: POST to /api/admin/creategroup
  * @DESC: Allows for an admin to create a new group with passed in info
  * @ACCESS: admin only
  */
-router.post('/creategroup', (req, res, next) => {
+router.post('/create_group', (req, res, next) => {
   const { name, description } = req.body;
   Group.create({ name, description })
     .then(response => res.send(response))
@@ -18,8 +18,22 @@ router.post('/creategroup', (req, res, next) => {
  * @DESC: Allows for an admin to add a chore to their specific group
  * @ACCESS: admin only
  */
-router.post('/addchore', (req, res, next) => {
+router.post('/add_chore', async (req, res, next) => {
   const { userId, name, difficulty, timeLimit, details } = req.body;
+  const userInfo = await UserGroup.findOne({ where: { userId } });
+  if (userInfo.userIsAdmin) {
+    Chore.create({
+      name,
+      difficulty,
+      timeLimit,
+      details,
+      groupId: userInfo.groupId,
+    });
+    // .then(newChore => res.send('created'))
+    // .catch(e => console.error(e));
+  } else {
+    res.status(404).send('You are not an admin');
+  }
 });
 
 module.exports = router;
