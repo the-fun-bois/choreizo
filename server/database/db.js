e63681f08b7ff8039d3758f27264904d278a411f
Kevin <kyu199023@gmail.com>
2019-08-28 00:01:53 -0400

    .travis.yml server
    postgres ci env var

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,9 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+const DATABASE_URL = process.env.DATABASE_URL || process.env.POSTGRES_CI
+
 const Sequelize = require('sequelize');
-const db = new Sequelize(process.env.DATABASE_URL, { logging: false });
+const db = new Sequelize(DATABASE_URL, { logging: false });
 
 module.exports = db;
