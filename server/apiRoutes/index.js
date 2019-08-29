dc20de4dd640ac874794230d017d97aeb7f4fafb
Ryan Jasinski <jasinski.17@gmail.com>
2019-08-28 20:35:48 -0400

    comment out Chores API importt MUST MOVE BACK

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 
 const apiRoutes = express.Router();
-// import api routes here
+//import api routes here
 
 const tradeChoreRoutes = require('./tradeChoreRoutes');
 const swapChoreRoutes = require('./swapChoreRoutes');
 const transferChoreRoutes = require('./transferChoreRoutes');
 const adminRoutes = require('./adminRoutes');
-const choresAPIRoutes = require('./choresRoutes');
+const choresRoutes = require('./choresRoutes');
 
 // set api routes here
 apiRoutes.use('/trade_chore', tradeChoreRoutes);
 apiRoutes.use('/swap_chore', swapChoreRoutes);
 apiRoutes.use('/transfer_chore', transferChoreRoutes);
-apiRoutes.use('/chores', choresAPIRoutes);
+apiRoutes.use('/chores', choresRoutes);
 apiRoutes.use('/admin', adminRoutes);
 
 module.exports = apiRoutes;
