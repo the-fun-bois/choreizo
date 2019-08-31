e509d04d242c84925eeed3392531a8a12c75358f
Kevin <kyu199023@gmail.com>
2019-08-31 15:58:00 -0400

    auth middleware

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -8,6 +8,11 @@ const swapChoreRoutes = require('./swapChoreRoutes');
 const transferChoreRoutes = require('./transferChoreRoutes');
 const choresRoutes = require('./ChoresRoutes');
 // set api routes here
+
+apiRoutes.use('*', (req, res, next) => {
+  if (req.isAuthenticated()) next();
+  else res.sendStatus(400);
+});
 apiRoutes.use('/trade_chore', tradeChoreRoutes);
 apiRoutes.use('/swap_chore', swapChoreRoutes);
 apiRoutes.use('/transfer_chore', transferChoreRoutes);
