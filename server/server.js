2c8560da40d3fa5eff8d7f3cdc8e1f5fb75aa6b8
Kevin <kyu199023@gmail.com>
2019-08-31 15:04:34 -0400

    server oauth

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
+require('./config/passportConf');
 const express = require('express');
 const morgan = require('morgan');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const path = require('path');
 const chalk = require('chalk');
+const auth = require('./config/auth');
 
 const app = express();
 
@@ -12,9 +16,18 @@ app.use(morgan(morganMode));
 
 app.use(express.json());
 
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [auth.session.cookieKey]
+}));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(express.urlencoded({ extended: false }));
-}
+};
 
 const apiRoutes = require('./apiRoutes');
 
