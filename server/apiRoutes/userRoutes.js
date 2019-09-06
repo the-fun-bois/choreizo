9db9356a328263a7be415ffb8f047fe19cbcf357
hypedis <m.bae@live.com>
2019-09-06 12:26:11 -0400

    added user post route

--- server/apiRoutes/userRoutes.js
+++ server/apiRoutes/userRoutes.js
@@ -5,11 +5,20 @@ router.get('/profile', (req, res, next) => {
   User.findByPk(req.body.userId)
     .then(user => {
       if (!user) {
-        return res
-          .status(400)
-          .send({ error: 'User not found', userId: req.body.userId });
+        return res.status(400).send({ error: 'User not found' });
       }
-      res.status(200).send(user);
+      return res.status(200).send(user);
+    })
+    .catch(next);
+});
+
+router.post('/profile', (req, res, next) => {
+  User.findByPk(req.body.userId)
+    .then(user => {
+      if (!user) {
+        return res.status(400).send({ error: 'User not found' });
+      }
+      return res.status(200).send(user);
     })
     .catch(next);
 });
