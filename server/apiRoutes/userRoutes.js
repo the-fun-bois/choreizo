d4e24a4f327efd33bbb78f71845be6ac11bce032
hypedis <m.bae@live.com>
2019-09-06 12:33:04 -0400

    added usergroup

--- server/apiRoutes/userRoutes.js
+++ server/apiRoutes/userRoutes.js
@@ -1,19 +1,8 @@
 const router = require('express').Router();
-const { User } = require('../database');
+const { User, Group } = require('../database');
 
 router.get('/profile', (req, res, next) => {
-  User.findByPk(req.body.userId)
-    .then(user => {
-      if (!user) {
-        return res.status(400).send({ error: 'User not found' });
-      }
-      return res.status(200).send(user);
-    })
-    .catch(next);
-});
-
-router.post('/profile', (req, res, next) => {
-  User.findByPk(req.body.userId)
+  User.findByPk(req.body.userId, { include: [Group] })
     .then(user => {
       if (!user) {
         return res.status(400).send({ error: 'User not found' });
