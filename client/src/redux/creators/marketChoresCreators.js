38c8cb756e6271677407d0beb223475ff3ccabf8
Ryan Jasinski <jasinski.17@gmail.com>
2019-09-08 12:49:20 -0400

    updated and working from rebase disaster

--- client/src/redux/creators/marketChoresCreators.js
+++ client/src/redux/creators/marketChoresCreators.js
@@ -7,13 +7,13 @@ export const gotMarketChores = marketChores => ({
   marketChores,
 });
 
-export const getMarketChoresThunk = () => {
-  return (dispatch, getState) => {
-    if (!getState().userInfo.groups[0]) {
-      return;
-    }
-    const groupId = getState().userInfo.groups[0].id;
-    const userId = getState().userInfo.id;
+export const getMarketChoresThunk = groupId => {
+  return dispatch => {
+    // if (!getState().userInfo.groups[0]) {
+    //   return;
+    // }
+    // const groupId = getState().userInfo.groups[0].id;
+    // const userId = getState().userInfo.id;
     if (!groupId) {
       dispatch(gotMarketChores([]));
     } else {
@@ -27,15 +27,5 @@ export const getMarketChoresThunk = () => {
           console.log('error fetching market chores', e);
         });
     }
-
-    return serverApi
-      .post('/chores/market_chores', { userId, groupId })
-      .then(response => {
-        const marketChores = response.data;
-        dispatch(gotMarketChores(marketChores));
-      })
-      .catch(e => {
-        console.log('error fetching market chores', e);
-      });
   };
 };
