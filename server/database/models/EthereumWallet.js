086c2d7a8c71fd56a1394cb89d944cc9230186d2
hypedis <m.bae@live.com>
2019-09-09 01:10:52 -0400

    changed decimals to integers

--- server/database/models/EthereumWallet.js
+++ server/database/models/EthereumWallet.js
@@ -3,8 +3,9 @@ const db = require('./../db');
 
 const EthereumWallet = db.define('ethereumWallet', {
   balance: {
-    type: Sequelize.DECIMAL(10, 8),
+    type: Sequelize.INTEGER,
     allowNull: false,
+    defaultValue: 0,
   },
 });
 
