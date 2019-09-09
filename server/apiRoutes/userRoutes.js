bd15f9878e53e8a16d9c73b74af44a230dc87a85
Kevin <kyu199023@gmail.com>
2019-09-09 19:34:01 -0400

    user put route

--- server/apiRoutes/userRoutes.js
+++ server/apiRoutes/userRoutes.js
@@ -7,4 +7,20 @@ router.get('/profile', (req, res, next) => {
     .catch(next);
 });
 
+router.put('/update', (req, res, next) => {
+  const firstName = req.body.firstName;
+  const surName = req.body.surName;
+  User.findByPk(req.body.userId)
+    .then(user => {
+      return user.update({
+        firstName,
+        surName,
+      })
+    })
+    .then(updatedUser => {
+      res.json(updatedUser);
+    })
+    .catch(next);
+});
+
 module.exports = router;
