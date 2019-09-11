4e7d8acc2074c3af4012ba020bf58249dce38964
hypedis <m.bae@live.com>
2019-09-10 20:56:44 -0400

    changed chores and user names

--- server/database/seed/data/usersSeed.js
+++ server/database/seed/data/usersSeed.js
@@ -3,38 +3,29 @@ const usersSeed = [
     {
       email: 'fsacapstone001@gmail.com',
       imageUrl:
-        'https://lh6.googleusercontent.com/-Zih8BlwtLag/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rdEopJwRBLQmMjue8BZHKap-5MeDw/photo.jpg',
-      firstName: 'Master',
-      surName: 'Chief',
+        'http://1ryzas42x65e2oosia40bgli.wpengine.netdna-cdn.com/wp-content/uploads/2017/10/2017-10-25-1-1.jpg',
+      firstName: 'Tom',
+      surName: 'Hanks',
     },
     {
-      firstName: 'Kevin',
-      surName: 'Yu',
-      email: 'kevin@gmail.com',
-      imageUrl:
-        'https://cdn.xl.thumbs.canstockphoto.com/cattail-detailed-illustration-background-clip-art-vector_csp19117867.jpg',
+      firstName: 'Jackie',
+      surName: 'Chan',
+      email: 'jackie@gmail.com',
+      imageUrl: 'http://www.jackiechankids.com/images/long-hair2.jpg',
     },
     {
-      firstName: 'Naimun',
-      surName: 'Siraj',
-      email: 'naimun@gmail.com',
-      imageUrl:
-        'https://cdn.xl.thumbs.canstockphoto.com/cattail-detailed-illustration-background-clip-art-vector_csp19117867.jpg',
+      firstName: 'Zayn',
+      surName: 'Malik',
+      email: 'zayn.onedirection@gmail.com',
+      imageUrl: 'https://d.newsweek.com/en/full/983997/zayn-malik.png',
     },
 
     {
-      firstName: 'Mark',
-      surName: 'Bae',
-      email: 'mark@gmail.com',
+      firstName: 'Jeremy',
+      surName: 'Lin',
+      email: 'linsanity@gmail.com',
       imageUrl:
-        'https://cdn.xl.thumbs.canstockphoto.com/cattail-detailed-illustration-background-clip-art-vector_csp19117867.jpg',
-    },
-    {
-      firstName: 'Ryan',
-      surName: 'Jasinski',
-      email: 'ryan@gmail.com',
-      imageUrl:
-        'https://cdn.xl.thumbs.canstockphoto.com/cattail-detailed-illustration-background-clip-art-vector_csp19117867.jpg',
+        'https://sportsspectrum.s3.amazonaws.com/live/wp-content/uploads/2012/11/02092122/HOU_Lin_Jeremy.png',
     },
   ],
   [
